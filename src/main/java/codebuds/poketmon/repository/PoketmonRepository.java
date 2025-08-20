5098bb6cbf273d31fae0159cf0375f9e95d6561c
HyeonggyuKang <kin0204@naver.com>
2025-08-20 17:32:37 +0900

    기능 개발 및 테스트 완료
    1. 회원 가입 , 탈퇴
    2. 포켄몬 포획, 방출
    3. 파일 입출력 및 동기화
    4. 기준 정보생성(기본 포켓몬 4마리)

--- src/main/java/codebuds/poketmon/repository/PoketmonRepository.java
+++ src/main/java/codebuds/poketmon/repository/PoketmonRepository.java
@@ -10,53 +10,67 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class PoketmonRepository implements Serializable {
+public class PoketmonRepository {
 
     private File poketFile = new File("src/main/java/codebuds/poketmon/db/poketMonster.dat");
     private File memberFile = new File("src/main/java/codebuds/poketmon/db/member.dat");
     private List<PoketMon> poketMons = new ArrayList<>();
     private List<Member> members = new ArrayList<Member>();
 
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public List<PoketMon> getMPoketMons() {
+        return poketMons;
+    }
+
     public PoketmonRepository() {
 
         if (!memberFile.exists()) {
             members.add(new Member(1, "이승건", "이승건", 26, new ArrayList<>(), AccountStatus.ACTIVE));
             members.add(new Member(2, "강형규", "강형규", 34, new ArrayList<>(), AccountStatus.ACTIVE));
+            members.add(new Member(3, "TESTUSER", "TESTUSER", 34, new ArrayList<>(), AccountStatus.ACTIVE));
             saveListData(1, members);
+        } else {
+            searchMembers();
+            System.out.println(members);
         }
 
         if (!poketFile.exists()) {
             initPoketMonster(poketMons);
             saveListData(2, poketMons);
+        } else {
+            searchPoketMons();
+            System.out.println(poketMons);
         }
     }
 
     private void initPoketMonster(List<PoketMon> poketMons) {
         poketMons.add(new PoketMon(1, "피카츄", 25, 0.45, 0.3
-                , new ArrayList<Skil>() {{
-            add(new Skil("백만볼트", 100));
-            add(new Skil("몸통박치기", 50));
-        }}
-        ));
-        poketMons.add(new PoketMon(1, "이브이", 5, 0.5, 0.2
-                , new ArrayList<Skil>() {{
-            add(new Skil("점프", 0));
-            add(new Skil("몸통박치기", 30));
-        }}
-        ));
-        poketMons.add(new PoketMon(1, "꼬부기", 10, 0.65, 0.34
-                , new ArrayList<Skil>() {{
-            add(new Skil("물대포", 80));
-            add(new Skil("몸통박치기", 40));
-        }}
-        ));
-        poketMons.add(new PoketMon(1, "파이리", 15, 0.55, 0.3
-                , new ArrayList<Skil>() {{
-            add(new Skil("불꽃뿜기", 40));
-            add(new Skil("몸통박치기", 30));
-        }}
-        ));
+                , Arrays.asList(
+                new Skil("백만볼트", 100),
+                new Skil("몸통박치기", 50)
+        )));
+
+        poketMons.add(new PoketMon(2, "이브이", 5, 0.5, 0.2
+                , Arrays.asList(
+                new Skil("점프", 0),
+                new Skil("몸통박치기", 30)
+        )));
+
+        poketMons.add(new PoketMon(3, "꼬부기", 10, 0.65, 0.34
+                , Arrays.asList(
+                new Skil("물대포", 80),
+                new Skil("몸통박치기", 40)
+        )));
+        poketMons.add(new PoketMon(4, "파이리", 15, 0.55, 0.3
+                , Arrays.asList(
+                new Skil("불꽃뿜기", 40),
+                new Skil("몸통박치기", 30)
+        )));
     }
+
 
     // 1 : Member 파일
     // 2 : PoketMonster 파일
@@ -76,7 +90,7 @@ public class PoketmonRepository implements Serializable {
                 }
             } else if (fileNum == 2) {
                 for (Object o : datas) {
-                    os.writeObject((PoketMon) o);
+                    os.writeObject((PoketMon)o);
                 }
             }
         } catch (IOException e) {
@@ -93,13 +107,104 @@ public class PoketmonRepository implements Serializable {
     public int removeMember(int id) {
         int result = 0;
         for (Member m : members) {
-            if(m.getUserNum() == id){
+            if (m.getUserNum() == id) {
                 m.setAccountStatus(AccountStatus.DEACTIVATED);
                 result = 1;
                 break;
             }
         }
+        saveListData(1, members);
 
         return result;
     }
+
+    public int registeMember(Member registMember) {
+        members.add(registMember);
+        saveListData(1, members);
+
+        return 1;
+    }
+
+    public int findMemberNumber() {
+        return members.get(members.size() - 1).getUserNum() + 1;
+    }
+
+    public int catchPoketMon(Member member, PoketMon poketMon) {
+        int result = 0;
+
+        for (Member m : members) {
+            if (m.getUserNum() == member.getUserNum()) {
+                m.getMyPoketMons().add(poketMon);
+                result = 1;
+                saveListData(1, members);
+                break;
+            }
+        }
+        return result;
+    }
+
+    public int releasePoketMon(Member member, int index) {
+        int result = 0;
+
+        for (Member m : members) {
+            if (m.getUserNum() == member.getUserNum()) {
+                m.getMyPoketMons().remove(index);
+                result = 1;
+                saveListData(1, members);
+                break;
+            }
+        }
+        return result;
+    }
+
+    public void searchMembers() {
+        ObjectInputStream ois = null;
+
+        try {
+            ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(memberFile)));
+            members.clear();
+            while (true) {
+                members.add((Member) ois.readObject());
+            }
+        } catch (EOFException e) {
+            System.out.println("회원 정보 읽어오기 완료.");
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+
+    public void searchPoketMons() {
+        ObjectInputStream ois = null;
+
+        try {
+            ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(poketFile)));
+            poketMons.clear();
+            while (true) {
+                poketMons.add((PoketMon) ois.readObject());
+            }
+        } catch (EOFException e) {
+            System.out.println("포켓몬 읽어오기 완료.");
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+
 }
--- src/main/java/codebuds/poketmon/run/Application.java
+++ src/main/java/codebuds/poketmon/run/Application.java
@@ -7,193 +7,193 @@ import java.util.Scanner;
 
 public class Application {
 
-    private static final PoketMosterGameService gs = new PoketMosterGameService();
-
-    public static void main(String[] args) {
-
-        Member loginMember = null;
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            int input;  // 입력을 받을 변수
-
-            if (loginMember == null) { // 로그인 전 화면
-                System.out.println("===== 포켓몬 게임 =====");
-                System.out.println("1. 회원 가입");
-                System.out.println("2. 로그인");
-                System.out.println("9. 게임 종료");
-
-                System.out.print("메뉴를 선택해주세요: ");
-                input = sc.nextInt();
-                switch (input) {
-                    case 1: // 회원 가입
-                        gs.registMember(signup());
-                        break;
-                    case 2: // 로그인
-                        loginMember = gs.loginMember(chooseMemNo());
-                        break;
-                    case 9: // 게임 종료
-                        System.out.println("게임을 종료합니다.");
-                        return;
-                    default:// 잘못된 입력
-                        System.out.println(input + "은 잘못된 입력입니다.");
-                }
-            } else { // 로그인 후 화면
-                System.out.println("===== 포켓몬 게임 =====");
-                System.out.println("1. 게임 시작");
-                System.out.println("2. 회원 탈퇴");
-                System.out.println("3. 로그아웃");
-                System.out.println("9. 게임 종료");
-
-                System.out.print("메뉴를 선택해주세요: ");
-                input = sc.nextInt();
-                switch (input) {
-                    case 1: // 게임 시작
-                        gameStart(loginMember);
-                        break;
-                    case 2: // 회원 탈퇴
-                        gs.deleteMember(loginMember);
-                        loginMember = null;
-                        break;
-                    case 3: // 로그아웃
-                        loginMember = null;
-                        break;
-                    case 9: // 게임 종료
-                        System.out.println("게임을 종료합니다.");
-                        return;
-                    default:// 잘못된 입력
-                        System.out.println(input + "은 잘못된 입력입니다.");
-                }
-            }
-
-        }
-
-    }
-
-    // 추후 유효한 번호인지 확인 필요 or 로그인 시 사용하므로 id pw 입력으로 변경
-    private static int chooseMemNo() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("해당 회원 번호를 입력하세요: ");
-        return sc.nextInt();
-    }
-
-    // 추후 회원가입 시 사용하므로 겹치는 ID가 있는지 확인 필요
-    private static Member signup() {
-        Scanner sc = new Scanner(System.in);
-        Member member = null;
-
-        System.out.print("ID를 입력해주세요: ");
-        String name = sc.nextLine();
-
-        System.out.print("PW를 입력해주세요: ");
-        String pw = sc.nextLine();
-
-        System.out.print("나이를 입력해주세요: ");
-        int age = sc.nextInt();
-
-        member = new Member(name, pw, age);
-
-        return member;
-    }
-
-    //게임 화면
-    private static void gameStart(Member member) {
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("===== 포켓몬 게임 =====");
-            System.out.println("1. 포켓몬 찾기(탐색)");
-            System.out.println("2. 내 포켓몬 목록");
-            System.out.println("9. 메인 메뉴 돌아가기");
-
-            System.out.print("메뉴를 선택해주세요: ");
-            int input = sc.nextInt();
-            switch (input) {
-                case 1:
-                    PoketMon pm = gs.findPoketMon();    // 포켓몬 탐색
-                    if (pm != null) battleScreen(member,pm);      // 포켓몬을 찾았으면 -> 전투 화면
-                case 2:                                 // 내 포켓몬 목록 보기
-                    selectMyPoketMon(member);           // 포켓몬 목록용 화면
-                case 9:
-                    System.out.println("메인 메뉴로 돌아갑니다.");
-                    return;
-                default:
-                    System.out.println(input + "은 잘못된 입력입니다.");
-            }
-
-        }
-    }
-
-    // 전투 화면
-    private static void battleScreen(Member member,PoketMon pm) {
-        Scanner sc = new Scanner(System.in);
-        int count = 0;  // 몬스터볼을 던진 횟수 체크해서 일정치 넘어가면 종료
-        boolean isCatch = false;
-        while (true) {
-            System.out.println("==== 전투 ====");
-            System.out.println("1. 몬스터볼 투척");
-            System.out.println("2. 도망가기");
-
-            System.out.print("메뉴를 선택해주세요: ");
-            int input = sc.nextInt();
-            switch (input) {
-                case 1:
-                    System.out.println("몬스터 볼을 던집니다.");
-                    isCatch = gs.catchPoketMon(pm);     // 몬스터 볼 투척해서 성공 or 실패
-                    count++;
-                    break;
-                case 2:
-                    System.out.println(pm.getName() + "으로부터 도망칩니다.");
-                    return;
-                default:
-                    System.out.println(input + "은 잘못된 입력입니다.");
-            }
-
-            if (isCatch == true) {
-                member = gs.updateMyPoketMon(pm);
-                System.out.println(pm.getName() + "을 성공적으로 잡았습니다.");
-                return;
-            }
-            if (count == 3) {
-                System.out.println("몬스터볼을 모두 소진했습니다.");
-                return;
-            }
-        }
-
-    }
-
-    //내 포켓몬 목록 화면
-    private static void selectMyPoketMon(Member member) {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("==== 내 포멧몬 목록 ====");
-            for (int i = 0; i < member.getMyPoketMons().size(); i++) {        // 내 포켓몬 길이
-                System.out.println((i + 1) + ". " + member.getMyPoketMons().get(i));
-            }
-            System.out.println("==== 내 포켓몬 목록 ====");
-
-            System.out.println("1. 포켓몬 방생");
-            System.out.println("2. 포켓몬 세부 정보 열람");
-            System.out.println("3. 돌아가기");
-
-            System.out.print("메뉴를 선택해주세요: ");
-            int input = sc.nextInt();
-            switch (input) {
-                case 1:
-                    System.out.print("방생할 포켓몬의 번호를 입력하세요: ");
-                    input = sc.nextInt();
-                    member = gs.removeMyPoketMon(member, input - 1);    // 변경된 정보를 member에 적용해야 하므로
-                    break;
-                case 2:
-                    System.out.println("열람할 포켓몬의 번호를 입력하세요: ");
-                    input = sc.nextInt();
-                    PoketMon p = gs.selectMyPoketMon(member, input - 1);
-                    System.out.println(p.toString());
-                case 3:
-                    System.out.println("게임 화면으로 돌아갑니다.");
-                    return;
-                default:
-                    System.out.println(input + "은 잘못된 입력입니다.");
-            }
-        }
-    }
+//    private static final PoketMosterGameService gs = new PoketMosterGameService();
+//
+//    public static void main(String[] args) {
+//
+//        Member loginMember = null;
+//        Scanner sc = new Scanner(System.in);
+//        while (true) {
+//            int input;  // 입력을 받을 변수
+//
+//            if (loginMember == null) { // 로그인 전 화면
+//                System.out.println("===== 포켓몬 게임 =====");
+//                System.out.println("1. 회원 가입");
+//                System.out.println("2. 로그인");
+//                System.out.println("9. 게임 종료");
+//
+//                System.out.print("메뉴를 선택해주세요: ");
+//                input = sc.nextInt();
+//                switch (input) {
+//                    case 1: // 회원 가입
+//                        gs.registMember(signup());
+//                        break;
+//                    case 2: // 로그인
+//                        loginMember = gs.loginMember(chooseMemNo());
+//                        break;
+//                    case 9: // 게임 종료
+//                        System.out.println("게임을 종료합니다.");
+//                        return;
+//                    default:// 잘못된 입력
+//                        System.out.println(input + "은 잘못된 입력입니다.");
+//                }
+//            } else { // 로그인 후 화면
+//                System.out.println("===== 포켓몬 게임 =====");
+//                System.out.println("1. 게임 시작");
+//                System.out.println("2. 회원 탈퇴");
+//                System.out.println("3. 로그아웃");
+//                System.out.println("9. 게임 종료");
+//
+//                System.out.print("메뉴를 선택해주세요: ");
+//                input = sc.nextInt();
+//                switch (input) {
+//                    case 1: // 게임 시작
+//                        gameStart(loginMember);
+//                        break;
+//                    case 2: // 회원 탈퇴
+//                        gs.deleteMember(loginMember);
+//                        loginMember = null;
+//                        break;
+//                    case 3: // 로그아웃
+//                        loginMember = null;
+//                        break;
+//                    case 9: // 게임 종료
+//                        System.out.println("게임을 종료합니다.");
+//                        return;
+//                    default:// 잘못된 입력
+//                        System.out.println(input + "은 잘못된 입력입니다.");
+//                }
+//            }
+//
+//        }
+//
+//    }
+//
+//    // 추후 유효한 번호인지 확인 필요 or 로그인 시 사용하므로 id pw 입력으로 변경
+//    private static int chooseMemNo() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("해당 회원 번호를 입력하세요: ");
+//        return sc.nextInt();
+//    }
+//
+//    // 추후 회원가입 시 사용하므로 겹치는 ID가 있는지 확인 필요
+//    private static Member signup() {
+//        Scanner sc = new Scanner(System.in);
+//        Member member = null;
+//
+//        System.out.print("ID를 입력해주세요: ");
+//        String name = sc.nextLine();
+//
+//        System.out.print("PW를 입력해주세요: ");
+//        String pw = sc.nextLine();
+//
+//        System.out.print("나이를 입력해주세요: ");
+//        int age = sc.nextInt();
+//
+//        member = new Member(name, pw, age);
+//
+//        return member;
+//    }
+//
+//    //게임 화면
+//    private static void gameStart(Member member) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        while (true) {
+//            System.out.println("===== 포켓몬 게임 =====");
+//            System.out.println("1. 포켓몬 찾기(탐색)");
+//            System.out.println("2. 내 포켓몬 목록");
+//            System.out.println("9. 메인 메뉴 돌아가기");
+//
+//            System.out.print("메뉴를 선택해주세요: ");
+//            int input = sc.nextInt();
+//            switch (input) {
+//                case 1:
+//                    PoketMon pm = gs.findPoketMon();    // 포켓몬 탐색
+//                    if (pm != null) battleScreen(member,pm);      // 포켓몬을 찾았으면 -> 전투 화면
+//                case 2:                                 // 내 포켓몬 목록 보기
+//                    selectMyPoketMon(member);           // 포켓몬 목록용 화면
+//                case 9:
+//                    System.out.println("메인 메뉴로 돌아갑니다.");
+//                    return;
+//                default:
+//                    System.out.println(input + "은 잘못된 입력입니다.");
+//            }
+//
+//        }
+//    }
+//
+//    // 전투 화면
+//    private static void battleScreen(Member member,PoketMon pm) {
+//        Scanner sc = new Scanner(System.in);
+//        int count = 0;  // 몬스터볼을 던진 횟수 체크해서 일정치 넘어가면 종료
+//        boolean isCatch = false;
+//        while (true) {
+//            System.out.println("==== 전투 ====");
+//            System.out.println("1. 몬스터볼 투척");
+//            System.out.println("2. 도망가기");
+//
+//            System.out.print("메뉴를 선택해주세요: ");
+//            int input = sc.nextInt();
+//            switch (input) {
+//                case 1:
+//                    System.out.println("몬스터 볼을 던집니다.");
+//                    isCatch = gs.catchPoketMon(pm);     // 몬스터 볼 투척해서 성공 or 실패
+//                    count++;
+//                    break;
+//                case 2:
+//                    System.out.println(pm.getName() + "으로부터 도망칩니다.");
+//                    return;
+//                default:
+//                    System.out.println(input + "은 잘못된 입력입니다.");
+//            }
+//
+//            if (isCatch == true) {
+//                member = gs.updateMyPoketMon(pm);
+//                System.out.println(pm.getName() + "을 성공적으로 잡았습니다.");
+//                return;
+//            }
+//            if (count == 3) {
+//                System.out.println("몬스터볼을 모두 소진했습니다.");
+//                return;
+//            }
+//        }
+//
+//    }
+//
+//    //내 포켓몬 목록 화면
+//    private static void selectMyPoketMon(Member member) {
+//        Scanner sc = new Scanner(System.in);
+//        while (true) {
+//            System.out.println("==== 내 포멧몬 목록 ====");
+//            for (int i = 0; i < member.getMyPoketMons().size(); i++) {        // 내 포켓몬 길이
+//                System.out.println((i + 1) + ". " + member.getMyPoketMons().get(i));
+//            }
+//            System.out.println("==== 내 포켓몬 목록 ====");
+//
+//            System.out.println("1. 포켓몬 방생");
+//            System.out.println("2. 포켓몬 세부 정보 열람");
+//            System.out.println("3. 돌아가기");
+//
+//            System.out.print("메뉴를 선택해주세요: ");
+//            int input = sc.nextInt();
+//            switch (input) {
+//                case 1:
+//                    System.out.print("방생할 포켓몬의 번호를 입력하세요: ");
+//                    input = sc.nextInt();
+//                    member = gs.removeMyPoketMon(member, input - 1);    // 변경된 정보를 member에 적용해야 하므로
+//                    break;
+//                case 2:
+//                    System.out.println("열람할 포켓몬의 번호를 입력하세요: ");
+//                    input = sc.nextInt();
+//                    PoketMon p = gs.selectMyPoketMon(member, input - 1);
+//                    System.out.println(p.toString());
+//                case 3:
+//                    System.out.println("게임 화면으로 돌아갑니다.");
+//                    return;
+//                default:
+//                    System.out.println(input + "은 잘못된 입력입니다.");
+//            }
+//        }
+//    }
 }
